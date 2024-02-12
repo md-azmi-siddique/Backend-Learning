02d8dc66c023e6c90af34e31df1a8ae560641519
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-02-12 07:22:57 +0600

    dotenv implement

--- class-3/BroilarPlate-express-js-ES5/src/controllers/HelloController.js
+++ class-3/BroilarPlate-express-js-ES5/src/controllers/HelloController.js
@@ -1,6 +1,6 @@
-exports.Hello1=(req,res)=>{
+exports.Hello1 = (req,res)=>{
     return res.status(200).json({
         status:"OK",
         text:"Hello"
-    });;
+    });
 }
--- class-3/BroilarPlate-express-js-ES5/src/routes/api.js
+++ class-3/BroilarPlate-express-js-ES5/src/routes/api.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const WelcomeController = require("../controllers/WelcomeController");
-const HelloController = require("../controllers/HelloController");
+const {Hello1} = require("../controllers/HelloController");
+// const HelloController = require("../controllers/HelloController");
 
 
 router.get("/", WelcomeController.welcome)
@@ -10,8 +11,8 @@ router.get("/welcome1", WelcomeController.welcome1)
 router.get("/welcome2", WelcomeController.welcome2)
 
 //from record
-router.get("/hello1-get", HelloController.Hello1)
-// router.get("/", HelloController.Hello1)
-router.post("/hello1-post", HelloController.Hello1)
+router.get("/hello1-get", Hello1)
+
+router.post("/hello1-post", Hello1)
 
 module.exports=router;
